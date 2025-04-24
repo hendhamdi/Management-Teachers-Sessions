7ef905acdde53026571d8a1930b9a4053d8b89e1
unknown <hendh0487@gmail.com>
2025-04-24 17:12:39 +0100

    update .env

--- .history/src/outil/DatabaseUtil_20241027171538.java
+++
@@ -1,5 +0,0 @@
-package outil;
-
-public class DatabaseUtil {
-
-}
--- src/util/DatabaseConnection.java
+++ src/util/DatabaseConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/emploidutemps_db";
-    private static final String USER = "YourUserName";
-    private static final String PASSWORD = "YourPassword";
+    private static final String URL = "DB_URL";
+    private static final String USER = "DB_USER";
+    private static final String PASSWORD = "DB_PASSWORD";
 
     public static Connection getConnection() throws SQLException {
         try {
